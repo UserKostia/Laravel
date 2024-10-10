d9676c6ea66de6cbf71012011c2fda544e62370f
UserKostia <kostastriletski.com>
2024-10-10 21:47:50 +0300

    SCRUM-8 Create PostController and creating posts method

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('posts.index');
+    }
+}
