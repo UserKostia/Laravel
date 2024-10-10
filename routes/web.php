dd25eed03b1c7b65f6df062c90412f8b734545c5
UserKostia <kostastriletski.com>
2024-10-10 22:10:12 +0300

    SCRUM-9 Add posts/commentsroute

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('posts', PostController::class);
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
